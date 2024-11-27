1f0ce13fa35597f7b3d7c6ecd27753f45194c148
Revingo <122109751+Revingo@users.noreply.github.com>
2024-11-27 12:42:20 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # selfie-tw
+
+## Questa è la repository del progetto di Tecnologie Web dell'anno 2024/2025 dell'Alma Mater Studiorum, Università di Bologna
+
+### Autori: Alessandro D'Ambrosio, Arda Çebi, Emanuele Dessi
+
+## Dependencies:
+
+- Node.js: at least version 14, preferably 18
+
+To host the App locally run "npm run dev" on the terminal.
---
+++ src/scripts/App.jsx
@@ -0,0 +1,32 @@
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from "react-dom";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    //It caches the data
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <header>
+            <Link to="/">Selfie!</Link>
+          </header>
+        </div>
+        <Routes>{/*Empty at the moment*/}</Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
