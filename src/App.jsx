60cb8b8f8d0a41dc9dfd2a892dc4e2bd5864809d
Revingo <122109751+Revingo@users.noreply.github.com>
2024-12-01 15:03:58 +0100

    loginform first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom";
+import loginForm from "./pages/loginForm";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +19,9 @@ const App = () => {
       <QueryClientProvider client={queryClient}>
         <div>
           <header>
-            <Link to="/">Selfie!</Link>
+            <Link to="/" element={<loginForm />}>
+              Selfie!
+            </Link>
           </header>
         </div>
         <Routes>{/*Empty at the moment*/}</Routes>
---
+++ src/pages/loginForm.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import { FaUser, FaLock } from "react-icons/fa";
+
+const loginForm = () => {
+  return (
+    <div>
+      <form action="">
+        <h1>Login</h1>
+        <div className="input-box">
+          <input type="text" placeholder="Username" required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" required />
+          <FaLock className="icon" />
+        </div>
+        <div className="remember-me-button">
+          <label>
+            <input type="checkbox" />
+            Remember Me
+            <Link to="/register">Don't have an account?</Link>
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </label>
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default loginForm;
