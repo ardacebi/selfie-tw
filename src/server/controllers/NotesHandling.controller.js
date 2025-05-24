211bf68acb0755fd7d6e929dd7cc653d491e1ff1
Revingo <122109751+Revingo@users.noreply.github.com>
2025-05-24 18:46:23 +0200

    create marked routes

--- src/server/controllers/NotesHandling.controller.js
+++ src/server/controllers/NotesHandling.controller.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 const { Types } = mongoose;
 import NoteData from "../models/NoteData.model.js";
 import ProfileData from "../models/ProfileData.model.js";
+import { marked } from "marked";
 
 export const createNote = async (req, res) => {
-    const { title, creationDate, lastModifiedDate, body } = req.body;
-    
+  const { title, creationDate, lastModifiedDate, body } = req.body;
+
   if (!title || !body) {
     return res.status(400).json({
       success: false,
@@ -29,18 +30,88 @@ export const createNote = async (req, res) => {
     const savedNote = await newNoteData.save();
     const updatedProfileData = await ProfileData.findByIdAndUpdate(
       id,
-      { $push : {ownedNotes: savedNote._id } },
+      { $push: { ownedNotes: savedNote._id } },
       { new: true },
     );
 
     if (!updatedProfileData) {
       await NoteData.findByIdAndDelete(savedNote._id);
-      return res.status(404).json({ success: false, message: "User not found, note not created" });
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found, note not created" });
     }
 
     res.status(200).json({ success: true, data: updatedProfileData });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error", error: error.message });
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: error.message });
+  }
+};
+
+export const getAllUserNotes = async (req, res) => {
+  const { id } = req.params;
+
+  if (!Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid User ID" });
   }
 
-}
+  try {
+    const profile = await ProfileData.findById(id);
+
+    if (!profile) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    const profileNotes = profile.ownedNotes;
+
+    const notes = await NoteData.find({ _id: { $in: profileNotes } });
+    const result = notes.map((n) => ({
+      _id: n._id,
+      title: n.title,
+      creationDate: n.creationDate,
+      lastModifiedDate: n.lastModifiedDate,
+      HTMLbody: marked.parse(n.body), // Convert Markdown to HTML
+    }));
+
+    res.status(200).json({ success: true, data: result });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: error.message });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  const { noteId } = req.body;
+
+  if (!Types.ObjectId.isValid(noteId)) {
+    return res.status(404).json({ success: false, message: "Invalid Note ID" });
+  }
+
+  try {
+    const note = await NoteData.findById(noteId);
+    if (!note) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Note not found" });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: note._id,
+        title: note.title,
+        creationDate: note.creationDate,
+        lastModifiedDate: note.lastModifiedDate,
+        HTMLbody: marked.parse(note.body), // Convert Markdown to HTML
+      },
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Server Error", error: error.message });
+  }
+};
--- src/server/routes/NotesHandling.route.js
+++ src/server/routes/NotesHandling.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import {
-  createNote
+  createNote,
+  getAllUserNotes,
+  getNoteById,
 } from "../controllers/NotesHandling.controller.js";
 
 const router = Router();
 
 router.post("/create_note/:id", createNote);
+router.get("/get_all_user_notes/:id", getAllUserNotes);
+router.get("/get_note_by_id", getNoteById);
 
 export default router;
